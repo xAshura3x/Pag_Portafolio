c323cc253acc829d69ff9da61643a2255eb0a184
xAshura3x <osvaldo.antonio.marchant.gonzalez@gmail.com>
2026-03-23 20:13:11 -0300

    Cambios en el carrusel y eliminacion de una parte

--- Scripts/Carrusel.js
+++ Scripts/Carrusel.js
@@ -1,19 +1,39 @@
 const Imagenes = [
-    "../public/Img_prueba1.webp",
-    "../public/Img_prueba2.jpg",
-    "../public/Img_prueba3.png"
+    {
+        img: "../public/Img_prueba1.webp",
+        titulo: "Prueba1",
+        texto: "prueba1"
+    },
+    {
+        img: "../public/Img_prueba2.jpg",
+        titulo: "Prueba2",
+        texto: "Prueba2"
+    },
+    {
+        img: "../public/Img_prueba3.png",
+        titulo: "Prueba3",
+        texto: "Prueba3"
+    }
 ];
 
 let index = 0;
 
 const Img = document.getElementById("Img_Carrusel");
+const titulo = document.getElementById("Titulo_overlay");
+const texto = document.getElementById("Texto_overlay");
 
 function Cambiar_imagenes(){
+
+    if(!Imagenes[index] || !Imagenes[index].img) return;
 
     Img.style.opacity = 0;
 
     setTimeout(() =>{
-        Img.src = Imagenes[index];
+        Img.src = Imagenes[index].img;
+
+        titulo.textContent = Imagenes[index].titulo;
+        texto.textContent = Imagenes[index].texto;
+
         Img.style.opacity = 1;
     }, 200);
 }
